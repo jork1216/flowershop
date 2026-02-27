2f8c70d7de4d0f8103881e9c846edf2ba1006c10
junkcarlo16 <ramosjohncarlo16@gmail.com>
2026-02-27 15:21:02 +0800

    added products

---
+++ src/data/products.js
@@ -0,0 +1,10 @@
+// src/data/products.js
+
+export const bestSellerProducts = [
+  { id: 1, name: "Wine",     price: "P890.00",   oldPrice: "P990.00"   },
+  { id: 2, name: "Blush",    price: "P890.00",   oldPrice: "P990.00"   },
+  { id: 3, name: "Flamingo", price: "P890.00",   oldPrice: "P990.00"   },
+  { id: 4, name: "Kiss",     price: "P1,890.00", oldPrice: "P1,990.00" },
+  { id: 5, name: "Cherry",   price: "P2,890.00", oldPrice: "P2,990.00" },
+  { id: 6, name: "Dream",    price: "P1,890.00", oldPrice: "P1,990.00" },
+];
