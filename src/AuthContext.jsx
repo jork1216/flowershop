bb87ae22d42b30897c4cfe3184394c8eb82cfe75
junkcarlo16 <ramosjohncarlo16@gmail.com>
2026-03-13 12:44:46 +0800

    added firebase

---
+++ src/AuthContext.jsx
@@ -0,0 +1,17 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../firebase";
+
+const AuthContext = createContext(null);
+
+export function AuthProvider({ children }) {
+  const [user, setUser] = useState(undefined);
+
+  useEffect(() => {
+    return onAuthStateChanged(auth, setUser);
+  }, []);
+
+  return <AuthContext.Provider value={user}>{children}</AuthContext.Provider>;
+}
+
+export const useAuth = () => useContext(AuthContext);
